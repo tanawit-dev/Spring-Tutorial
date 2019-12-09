8d977371a250735dc9a488aa482f05d9e68c3d81
tanawit.dev <tanawit.dev@hotmail.com>
2019-12-09 17:23:27 +0700

    Add view controller

--- src/main/java/com/spring/example/controller/ViewController.java
+++ src/main/java/com/spring/example/controller/ViewController.java
@@ -1,4 +1,17 @@
 package com.spring.example.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
 public class ViewController {
+    @RequestMapping("/view-products")
+    public String viewProducts() {
+        return "view-products";
+    }
+
+    @RequestMapping("/add-products")
+    public String addProducts() {
+        return "add-products";
+    }
 }
