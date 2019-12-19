fa4cb1cbbc6f660e816c59b1ed22f8a3cf5714e3
Tanawit Aeabsakul <tanawit.dev@hotmail.com>
2019-12-19 23:21:27 +0700

    Start using flyway

---
+++ src/main/resources/db/migration/V1__init.sql.sql
@@ -0,0 +1,16 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ * Author:  g
+ * Created: Dec 19, 2019
+ */
+CREATE TABLE book (
+    id int NOT NULL AUTO_INCREMENT,
+    title varchar(255),
+    author varchar(255),
+    PRIMARY KEY(id)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8
+
